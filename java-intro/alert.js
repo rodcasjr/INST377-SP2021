327c3383c1942b0b8ad3700fb78edb433317bd26
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-02-10 22:03:16 -0500

    update alt tags

---
+++ java-intro/alert.js
@@ -0,0 +1,28 @@
+alert ("I'm Javascript!");
+alert ('Hello');
+alert ('World');
+// comments do not effect its execution. single line comments.
+alert(3+
+    1
+    +2);
+/* multi line comments */
+
+let message;
+message = 'Hello!';//store the string
+alert(message);
+
+let n_message = 'Bye!';
+alert(n_message);
+
+let user ='John';
+let age = 25;
+let message ='Hello';
+
+let user ='John', 
+age =25, 
+message ='Bye';
+
+//comma first style
+let user ='John' 
+    , age =25, 
+    , message ='Bye';
