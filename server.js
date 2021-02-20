1d2440cb5cb6817d2b0f26d86b9a9c20374fc740
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-02-20 13:46:34 -0500

    commit on main

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.route('/api')
   .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    console.log('Now send something back to your client');
-    res.json({message: 'Hello World'});
+    //console.log('Now send something back to your client');
+    res.send({message: 'hello world'});
   });
 
 app.listen(port, async () => {
